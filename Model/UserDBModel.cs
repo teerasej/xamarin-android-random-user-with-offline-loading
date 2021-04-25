5493fec6ad20cd6be2a0760412bbfbe78a529462
Teerasej <teerasej@gmail.com>
2021-04-25 16:46:42 +0700

    สร้าง Class ตัวแทนของข้อมูล User ใน Database - กำหนด property ด้วย tag primary key และ auto increment - กำหนด column คู่กับ property ต่างๆ - สร้าง method สำหรับโหลดข้อมูลจาก Web API มาใส่

---
+++ Model/UserDBModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using SQLite;
+
+namespace RandomUser.Model
+{
+    public class UserDBModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [Column("first_name")]
+        public string FirstName { get; set; }
+
+        [Column("last_name")]
+        public string LastName { get; set; }
+
+        [Column("phone_number")]
+        public string PhoneNumber { get; set; }
+
+        [Column("email")]
+        public string Email { get; set; }
+
+        [Column("image_path")]
+        public string ImagePath { get; set; }
+
+        public UserDBModel()
+        {
+        }
+
+        public void ApplyDataFromRemoteModel(Result result)
+        {
+            this.FirstName = result.Name.First;
+            this.LastName = result.Name.Last;
+
+            this.PhoneNumber = result.Phone;
+            this.Email = result.Email;
+
+            this.ImagePath = result.Picture.Large.AbsoluteUri;
+        }
+    }
+}
