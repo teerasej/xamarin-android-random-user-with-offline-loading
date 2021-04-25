8dd5fd2a59cd5fb143c21724d9389927636f4f6d
Teerasej <teerasej@gmail.com>
2021-04-25 14:34:55 +0700

    สร้างส่วนการเตรียมข้อมูล Local - ในที่นี้จะเตรียมรูปภาพที่ต้องแสดงในแอพ - ใช้วิธีโหลดมาเก็บไว้ในเครื่องก่อน - หลังจากได้ข้อมูลผู้ใช้ทั้งหมดแล้ว ก็เริ่มวนโหลดรูปมาเก็บไว้ในแอพ - กำหนดที่อยู่สำหรับเก็บรูปภาพไว้ใน LocalApplicationData - ดาวน์โหลดไฟล์ด้วย web client - ถ้าการดาวน์โหลดสมบูรณ์ กำหนดที่อยู่ local ให้กับ user object - สุดท้ายคืนข้อมูล user ทั้งหมดออกไปใช้งาน

--- Service/UserService.cs
+++ Service/UserService.cs
@@ -2,6 +2,10 @@
 using System.Threading.Tasks;
 using RestSharp;
 using RandomUser.Model;
+using System.Net;
+using Android.App;
+using System.IO;
+using Result = RandomUser.Model.Result;
 
 namespace RandomUser.Service
 {
@@ -10,6 +14,7 @@ namespace RandomUser.Service
 
         private RestClient Client;
         private string BaseUrl = "https://randomuser.me/api/";
+        WebClient webClient;
 
         public UserService()
         {
@@ -18,10 +23,49 @@ namespace RandomUser.Service
 
         public async Task<Result[]> GetUserProfiles()
         {
-            var request = new RestRequest("", DataFormat.Json).AddParameter("results", 50);
+            var request = new RestRequest("", DataFormat.Json).AddParameter("results", 200);
             var response = await Client.ExecuteAsync(request);
             var userModel = UserRemoteModel.FromJson(response.Content);
             return userModel.Results;
+        }
+
+        public async Task<Result[]> PrepareOfflineData()
+        {
+            var users = await GetUserProfiles();
+
+            var imageStorePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+
+            webClient = new WebClient();
+
+            // loop through users list to download their's profile
+            foreach (var user in users)
+            {
+                var imageUri = user.Picture.Large;
+
+                var fileName = Path.GetFileName(imageUri.LocalPath);
+                var localImagePath = Path.Combine(imageStorePath, fileName);
+
+                if (localImagePath == null || File.Exists(localImagePath))
+                {
+                    continue;
+                }
+
+
+                var bytes = webClient.DownloadData(imageUri);
+                File.WriteAllBytes(localImagePath, bytes);
+
+                if (File.Exists(localImagePath))
+                {
+                    user.Picture.Large = new Uri(localImagePath);
+                }
+
+            }
+
+            // after finish download image file, save its paths, and user's info to db
+
+            // show user's info and image profile from database in listview
+
+            return users;
         }
     }
 }
