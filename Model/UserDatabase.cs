0a412a299f4823fecf71af677458c881bcde3c3f
Teerasej <teerasej@gmail.com>
2021-04-25 16:47:39 +0700

    สร้าง class สำหรับเชื่อมต่อและจัดการ database

---
+++ Model/UserDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLite;
+
+namespace RandomUser.Model
+{
+    public class UserDatabase
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public UserDatabase(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+        }
+    }
+}
