e60606ed3bac09d3ba5de6789a9326bcb2f559be
Teerasej <teerasej@gmail.com>
2021-04-25 13:45:51 +0700

    สร้าง UserService เพื่อแยกส่วนติดต่อกับ Web API
    - ใช้ RestClient ของ RestSharp
    - กำหนด BaseURL ของ Web API
    - กำหนด method ให้ทำงานแบบ Async ด้วย Task
    - ส่ง request พร้อม parameter เพื่อกำหนดจำนวน user
    - แปลง JSON ที่ได้จาก response เป็น class ที่เตรียมไว้
    - คืนค่าออกไปเฉพาะ Array ของข้อมูล User

---
+++ Service/UserService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using RestSharp;
+using RandomUser.Model;
+
+namespace RandomUser.Service
+{
+    public class UserService
+    {
+
+        private RestClient Client;
+        private string BaseUrl = "https://randomuser.me/api/";
+
+        public UserService()
+        {
+            this.Client = new RestClient(this.BaseUrl);
+        }
+
+        public async Task<Result[]> GetUserProfiles()
+        {
+            var request = new RestRequest("", DataFormat.Json).AddParameter("results", 50);
+            var response = this.Client.Get(request);
+            var userModel = UserRemoteModel.FromJson(response.Content);
+            return userModel.Results;
+        }
+    }
+}
