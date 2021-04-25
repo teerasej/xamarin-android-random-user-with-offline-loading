aae9fce628892c0ec87fe322fe0ac3f762724791
Teerasej <teerasej@gmail.com>
2021-04-25 13:17:11 +0700

    สร้าง ListView สำหรับแสดงรายชื่อ User

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace RandomUser
@@ -14,6 +15,8 @@ namespace RandomUser
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            var listView = FindViewById<ListView>(Resource.Id.listViewUser);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
